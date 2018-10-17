bf48cfb117d54b63d04d7e0de57dea55c0c846fa
Quynh HP <hophuongquynh92@gmail.com>
2018-10-17 17:50:24 +0700

    Updates

---
+++ precache-manifest.cb311a044f901023adeb7e8eb473f9c5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "38ac2e909f7d8b77867c293d0801bde4",
+    "url": "/static/media/mockup.38ac2e90.png"
+  },
+  {
+    "revision": "09d1e794604ddb9ffd17b9858f2373fa",
+    "url": "/static/media/doit.09d1e794.svg"
+  },
+  {
+    "revision": "bbae28d33a248f18be016862f1e591ac",
+    "url": "/static/media/avatar.bbae28d3.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "cd5b22453b7db69a6111",
+    "url": "/static/js/main.cd5b2245.chunk.js"
+  },
+  {
+    "revision": "ebc07b1ded3112895267",
+    "url": "/static/js/1.ebc07b1d.chunk.js"
+  },
+  {
+    "revision": "7a59e79dda741db9945e06eb0f3a2375",
+    "url": "/index.html"
+  }
+];
